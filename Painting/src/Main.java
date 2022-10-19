688a72f27505fa1466620cf79cc3030b18cb041d
mina-hydro <mina.hany.00k@gmail.com>
2022-10-19 16:08:10 +0200

    s

--- Painting/src/Main.java
+++ Painting/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
         frame.setVisible(true);
-        frame.setSize(1900, 830);
+        frame.setSize(1900, 900);
         frame.setContentPane(new Panel());
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
